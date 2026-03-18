8f3289045edbe45a737b19ceb0aecb86e00183ec
GuigohC0D3 <guilhermewerneckpereira11@hotmail.com>
2026-03-17 22:23:46 -0300

    ✨ feat(main): criação do módulo de atenção do transformer.

--- lab_p104/transformer.py
+++ lab_p104/transformer.py
@@ -3,33 +3,11 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 try:
+    from lab_p104.attention import AttentionUnit
     from lab_p104.components import FeedForwardBlock, ResidualNorm, SinusoidalPosition
 except ModuleNotFoundError:
+    from attention import AttentionUnit
     from components import FeedForwardBlock, ResidualNorm, SinusoidalPosition
-
-
-class AttentionUnit(nn.Module):
-    def __init__(self, d_model):
-        super().__init__()
-        self.q_proj = nn.Linear(d_model, d_model, bias=False)
-        self.k_proj = nn.Linear(d_model, d_model, bias=False)
-        self.v_proj = nn.Linear(d_model, d_model, bias=False)
-        self.o_proj = nn.Linear(d_model, d_model, bias=False)
-
-    def forward(self, query_source, key_value_source, attn_mask=None):
-        q = self.q_proj(query_source)
-        k = self.k_proj(key_value_source)
-        v = self.v_proj(key_value_source)
-
-        scale = q.size(-1) ** 0.5
-        score = torch.matmul(q, k.transpose(-2, -1)) / scale
-
-        if attn_mask is not None:
-            score = score + attn_mask
-
-        attn = F.softmax(score, dim=-1)
-        out = torch.matmul(attn, v)
-        return self.o_proj(out), attn
 
 
 class EncoderLayer(nn.Module):
